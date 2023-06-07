973c08d6e170f68a4a740f1c21a33f011667e62e
Zachary Kohnen <me@dusterthefirst.com>
2023-06-07 22:23:14 +0200

    Cross compilation and some structure I guess

---
+++ Justfile
@@ -0,0 +1,18 @@
+_:
+    just --list
+
+# Cross-compile the applocation for raspberry-pi
+build:
+    cross build
+
+pi_ip := "10.42.0.159"
+
+# Upload over SCP
+upload: build
+    scp target/armv7-unknown-linux-gnueabihf/debug/cameras aero@{{pi_ip}}:/home/aero/cameras/cameras
+
+set positional-arguments
+
+# Run over SSH
+run *args: build upload
+    ssh -t aero@{{pi_ip}} "/home/aero/cameras/cameras $@"
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use std::time::{Instant, SystemTime};
+use std::{
+    io,
+    ops::ControlFlow,
+    time::{Duration, Instant},
+};
 
 use clap::Parser;
 use log::{debug, error, info, trace, warn};
-use mavlink::{error::MessageReadError, MavHeader};
+use mavlink::error::MessageReadError;
+use serialport::TTYPort;
 use simplelog::TermLogger;
 use systemd_journal_logger::JournalLog;
 
@@ -43,31 +48,14 @@ fn main() {
 
     log::set_max_level(log::LevelFilter::Trace);
 
-    let camera_manager =
-        libcamera::camera_manager::CameraManager::new().expect("failed to initialize libcamera");
-
-    let camera_list = camera_manager.cameras();
-
-    for i in 0..camera_list.len() {
-        let camera = camera_list.get(i).unwrap();
-
-        let properties = camera.properties();
-
-        let location: libcamera::properties::Location = properties.get().unwrap();
-        let model: libcamera::properties::Model = properties.get().unwrap();
-        let properties: libcamera::properties::PixelArraySize = properties.get().unwrap();
-
-        debug!(
-            "{:?} {} @ {}x{}",
-            location, model.0, properties.0.width, properties.0.height
-        );
-    }
-
     // TODO:
     // serialport::available_ports();
 
     info!("connecting to serial port: {:?}", args.port);
-    let mut serial_port = match serialport::new(args.port, 115200).open_native() {
+    let serial_port = match serialport::new(args.port, 115200)
+        .timeout(Duration::from_millis(500))
+        .open_native()
+    {
         Ok(serial_port) => serial_port,
         Err(error) => {
             error!("failed to open serial port: {error}");
@@ -77,97 +65,149 @@ fn main() {
 
     info!("initialized, program will gracefully handle errors from now on");
 
-    let mut mavlink_header = {
-        let mut sequence = 0;
-        move || {
-            let header = mavlink::MavHeader {
-                system_id: 1,
-                component_id: mavlink::common::MavComponent::MAV_COMP_ID_CAMERA as u8,
-                sequence,
-            };
-
-            sequence += 1;
-
-            header
-        }
+    let mut connection = MavLinkConnection {
+        serial_port,
+        sequence_number: 0,
     };
 
-    let header = mavlink_header();
+    // Keep trying to handle mavlink commands, handling errors and retrying on timeouts
+    loop {
+        if let ControlFlow::Break(Err(error)) = handle_mavlink(&mut connection, system_start) {
+            todo!("{error}")
+        }
+    }
+}
+
+fn handle_mavlink(
+    connection: &mut MavLinkConnection,
+    system_start: Instant,
+) -> ControlFlow<Result<(), io::Error>, ()> {
     let message = mavlink_heartbeat_message();
-    match mavlink::write_v2_msg(&mut serial_port, header, &message) {
-        Ok(length) => {
-            debug!("TX[{length}]: {header:?} {message:?}");
-        }
-        Err(mavlink::error::MessageWriteError::Io(error)) => todo!(),
-    };
+    connection.write_gracefully(message)?;
 
-    match mavlink::read_v2_msg::<mavlink::common::MavMessage, _>(&mut serial_port) {
-        Err(MessageReadError::Io(error)) => todo!(),
-        Err(MessageReadError::Parse(error)) => todo!(),
-        Ok((header, message)) => {
-            debug!("RX: {header:?} {message:?}");
+    if let (header, mavlink::common::MavMessage::COMMAND_INT(command)) =
+        connection.read_gracefully()?
+    {
+        let acknowledge = |result| {
+            mavlink::common::MavMessage::COMMAND_ACK(mavlink::common::COMMAND_ACK_DATA {
+                command: command.command,
+                result,
+                target_component: header.component_id,
+                target_system: header.system_id,
+                ..Default::default()
+            })
+        };
 
-            match message {
-                mavlink::common::MavMessage::COMMAND_INT(command) => {
-                    {
-                        let header = mavlink_header();
-                        let message = mavlink::common::MavMessage::COMMAND_ACK(
-                            mavlink::common::COMMAND_ACK_DATA {
-                                command: command.command,
-                                result: mavlink::common::MavResult::MAV_RESULT_ACCEPTED,
-                                ..Default::default()
-                            },
-                        );
-                        match mavlink::write_v2_msg(&mut serial_port, header, &message) {
-                            Ok(length) => {
-                                debug!("TX[{length}]: {header:?} {message:?}");
-                            }
-                            Err(mavlink::error::MessageWriteError::Io(error)) => todo!(),
-                        };
-                    }
+        match command.command {
+            mavlink::common::MavCmd::MAV_CMD_REQUEST_CAMERA_INFORMATION => {
+                connection.write_gracefully(acknowledge(
+                    mavlink::common::MavResult::MAV_RESULT_ACCEPTED,
+                ))?;
 
-                    fn mav_str(text: &[u8]) -> [u8; 32] {
-                        assert!(text.len() <= 32, "text length must be less than 32 bytes");
-
-                        let mut buffer = [0u8; 32];
-
-                        buffer[0..text.len()].copy_from_slice(text);
-
-                        buffer
-                    }
-
-                    {
-                        let header = mavlink_header();
-                        let message = mavlink::common::CAMERA_INFORMATION_DATA {
-                            flags: mavlink::common::CameraCapFlags::CAMERA_CAP_FLAGS_CAPTURE_VIDEO,
-                            focal_length: 5.1,
-                            lens_id: 0,
-                            resolution_h: todo!(),
-                            resolution_v: todo!(),
-                            sensor_size_h: todo!(),
-                            sensor_size_v: todo!(),
-                            time_boot_ms: system_start.elapsed().as_millis() as u32,
-                            firmware_version: todo!(),
-                            model_name: mav_str(b"16MP IMX519 Quad-Camera Kit"),
-                            vendor_name: mav_str(b"arducam"),
-                            ..Default::default()
-                        };
-                        match mavlink::write_v2_msg(
-                            &mut serial_port,
-                            header,
-                            &mavlink::common::MavMessage::CAMERA_INFORMATION(message),
-                        ) {
-                            Ok(length) => {
-                                debug!("TX[{length}]: {header:?} {message:?}");
-                            }
-                            Err(mavlink::error::MessageWriteError::Io(error)) => todo!(),
-                        };
-                    }
-                }
-                _ => todo!(),
+                connection.write_gracefully(mavlink::common::MavMessage::CAMERA_INFORMATION(
+                    mavlink::common::CAMERA_INFORMATION_DATA {
+                        flags: mavlink::common::CameraCapFlags::CAMERA_CAP_FLAGS_CAPTURE_VIDEO,
+                        focal_length: 5.1,
+                        lens_id: 0,
+                        resolution_h: todo!(),
+                        resolution_v: todo!(),
+                        sensor_size_h: todo!(),
+                        sensor_size_v: todo!(),
+                        time_boot_ms: system_start.elapsed().as_millis() as u32,
+                        firmware_version: todo!(),
+                        model_name: mav_str(b"16MP IMX519 Quad-Camera Kit"),
+                        vendor_name: mav_str(b"arducam"),
+                        ..Default::default()
+                    },
+                ))?;
+            }
+            mavlink::common::MavCmd::MAV_CMD_CAMERA_STOP_TRACKING => todo!(),
+            mavlink::common::MavCmd::MAV_CMD_CAMERA_TRACK_POINT => todo!(),
+            mavlink::common::MavCmd::MAV_CMD_CAMERA_TRACK_RECTANGLE => todo!(),
+            mavlink::common::MavCmd::MAV_CMD_REQUEST_CAMERA_CAPTURE_STATUS => todo!(),
+            mavlink::common::MavCmd::MAV_CMD_REQUEST_CAMERA_IMAGE_CAPTURE => todo!(),
+            mavlink::common::MavCmd::MAV_CMD_REQUEST_CAMERA_SETTINGS => todo!(),
+            mavlink::common::MavCmd::MAV_CMD_RESET_CAMERA_SETTINGS => todo!(),
+            mavlink::common::MavCmd::MAV_CMD_SET_CAMERA_FOCUS => todo!(),
+            mavlink::common::MavCmd::MAV_CMD_SET_CAMERA_MODE => todo!(),
+            mavlink::common::MavCmd::MAV_CMD_SET_CAMERA_ZOOM => todo!(),
+            _ => {
+                connection.write_gracefully(acknowledge(
+                    mavlink::common::MavResult::MAV_RESULT_UNSUPPORTED,
+                ))?;
             }
         }
     };
+
+    ControlFlow::Continue(())
+}
+
+pub struct MavLinkConnection {
+    serial_port: TTYPort,
+    sequence_number: u8,
+}
+
+impl MavLinkConnection {
+    fn mavlink_header(&mut self) -> mavlink::MavHeader {
+        let header = mavlink::MavHeader {
+            system_id: 1,
+            component_id: mavlink::common::MavComponent::MAV_COMP_ID_CAMERA as u8,
+            sequence: self.sequence_number,
+        };
+
+        self.sequence_number = self.sequence_number.wrapping_add(1);
+
+        header
+    }
+
+    pub fn write_gracefully(
+        &mut self,
+        message: mavlink::common::MavMessage,
+    ) -> ControlFlow<Result<(), io::Error>, usize> {
+        let header = self.mavlink_header();
+
+        match mavlink::write_v2_msg(&mut self.serial_port, header, &message) {
+            Ok(length) => {
+                debug!("TX[{length}]: {header:?} {message:?}");
+                ControlFlow::Continue(length)
+            }
+            Err(mavlink::error::MessageWriteError::Io(error)) => {
+                ControlFlow::Break(match error.kind() {
+                    io::ErrorKind::TimedOut => Ok(()),
+                    _ => Err(error),
+                })
+            }
+        }
+    }
+
+    pub fn read_gracefully(
+        &mut self,
+    ) -> ControlFlow<Result<(), io::Error>, (mavlink::MavHeader, mavlink::common::MavMessage)> {
+        match mavlink::read_v2_msg::<mavlink::common::MavMessage, _>(&mut self.serial_port) {
+            Err(MessageReadError::Io(error)) => match error.kind() {
+                std::io::ErrorKind::TimedOut => ControlFlow::Break(Ok(())),
+                _ => ControlFlow::Break(Err(error)),
+            },
+            Err(MessageReadError::Parse(error)) => {
+                todo!("{error}")
+            }
+            Ok((header, message)) => {
+                debug!("RX: {header:?} {message:?}");
+
+                ControlFlow::Continue((header, message))
+            }
+        }
+    }
+}
+
+fn mav_str(text: &[u8]) -> [u8; 32] {
+    assert!(text.len() <= 32, "text length must be less than 32 bytes");
+
+    let mut buffer = [0u8; 32];
+
+    buffer[0..text.len()].copy_from_slice(text);
+
+    buffer
 }
 
 pub fn mavlink_heartbeat_message() -> mavlink::common::MavMessage {
